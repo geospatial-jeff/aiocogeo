c9bc6488d875d31cb739497c1d95381a53cf849a
Jeff Albrecht <geospatialjeff@gmail.com>
2020-05-23 18:10:17 -0500

    test local filesystem

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,18 @@
-import pytest
+import os
+
 import aiohttp
+import pytest
 
 from async_cog_reader import COGReader
 
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
 
 TEST_DATA = [
     "https://async-cog-reader-test-data.s3.amazonaws.com/0097d134-9be4-47f6-816d-edb77c9ed79e.tif",  # 3 band, JPEG ycbcr
     "https://async-cog-reader-test-data.s3.amazonaws.com/lzw_cog.tif",  # 3 band lzw (RGBA)
     "https://async-cog-reader-test-data.s3.amazonaws.com/webp_cog.tif",
+    os.path.join(DATA_DIR, "cog.tif")
 ]
 
 
