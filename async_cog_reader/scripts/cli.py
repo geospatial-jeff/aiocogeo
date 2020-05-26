909175df171145e175187fed3cfbd6ebef685a01
Jeff Albrecht <geospatialjeff@gmail.com>
2020-05-25 19:45:13 -0500

    add tile size statistics to info cli

--- async_cog_reader/scripts/cli.py
+++ async_cog_reader/scripts/cli.py
@@ -8,6 +8,7 @@ from async_cog_reader import COGReader
 
 app = typer.Typer()
 
+
 def coro(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -15,16 +16,25 @@ def coro(f):
 
     return wrapper
 
+
 def _make_bold(s, **kwargs):
     return typer.style(s, bold=True, **kwargs)
 
+
 def _create_ifd_table(ifds, start="\t"):
-    table = f"{start}{_make_bold('Id', underline=True):<20}{_make_bold('Size', underline=True):<27}{_make_bold('TileSize', underline=True):<30}"
+    table = (
+        f"{start}{_make_bold('Id', underline=True):<20}{_make_bold('Size', underline=True):<27}"
+        f"{_make_bold('BlockSize', underline=True):<26}{_make_bold('MinTileSize (KB)', underline=True):<33}"
+        f"{_make_bold('MaxTileSize (KB)', underline=True):<33}{_make_bold('MeanTileSize (KB)', underline=True):<33}"
+    )
     for idx, ifd in enumerate(ifds):
+        tile_sizes = [b / 1000 for b in ifd.TileByteCounts.value]
+        mean_tile_size = round(sum(tile_sizes) / len(tile_sizes), 3)
         size = f"{ifd.ImageWidth.value}x{ifd.ImageHeight.value}"
-        tile_size = f"{ifd.TileWidth.value}x{ifd.TileHeight.value}"
-        table += f"\n\t\t{idx:<8}{size:<15}{tile_size:<30}"
+        block_size = f"{ifd.TileWidth.value}x{ifd.TileHeight.value}"
+        table += f"\n\t\t{idx:<8}{size:<15}{block_size:<14}{min(tile_sizes):<21}{max(tile_sizes):<21}{mean_tile_size:<30}"
     return table
+
 
 @app.command()
 @coro
@@ -33,7 +43,7 @@ async def info(filepath: str):
     async with COGReader(filepath) as cog:
         profile = cog.profile
         typer.echo(
-        f"""
+            f"""
         {_make_bold("FILE INFO:", underline=True)} {_make_bold(filepath)}
 
           {_make_bold("PROFILE")}
@@ -49,13 +59,18 @@ async def info(filepath: str):
             {_make_bold("Internal mask:"):<{sep}} {cog.is_masked}
         """
         )
-        typer.echo(f"""\t  {_make_bold("IFD")}
+        typer.echo(
+            f"""\t  {_make_bold("IFD")}
             {_create_ifd_table(cog.ifds)}
-        """)
+        """
+        )
         if cog.is_masked:
-            typer.echo(f"""\t  {_make_bold("MASK IFD")}
+            typer.echo(
+                f"""\t  {_make_bold("MASK IFD")}
                 {_create_ifd_table(cog.mask_ifds, start="")}
-            """)
+            """
+            )
+
 
 @app.command()
 @coro
