dcdd1ab0bb1003752de259f5e8e060a26b1bc5f9
Jeff Albrecht <geospatialjeff@gmail.com>
2020-06-13 18:30:22 -0500

    add boundless read test cases

--- tests/test_cog_reader.py
+++ tests/test_cog_reader.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 from morecantile.models import TileMatrixSet
 import mercantile
@@ -14,7 +15,7 @@ from shapely.geometry import Polygon
 from aiocogeo import config
 from aiocogeo.ifd import IFD
 from aiocogeo.tag import Tag
-from aiocogeo.errors import InvalidTiffError
+from aiocogeo.errors import InvalidTiffError, TileNotFoundError
 
 from .conftest import TEST_DATA
 
@@ -283,6 +284,65 @@ async def test_cog_read_merge_range_requests_with_internal_nodata_mask(create_co
     assert bytes_requested == merged_bytes_requested
     assert tile_data.all() == tile_data_merged.all()
     assert tile_data.shape == tile_data_merged.shape
+
+
+@pytest.mark.asyncio
+async def test_boundless_read(create_cog_reader, monkeypatch):
+    infile = "http://async-cog-reader-test-data.s3.amazonaws.com/webp_web_optimized_cog.tif"
+    tile = mercantile.Tile(x=701, y=1634, z=12)
+    bounds = mercantile.xy_bounds(tile)
+
+    # Confirm an exception is raised if boundless reads are disabled
+    monkeypatch.setattr(config, "BOUNDLESS_READ", False)
+
+    async with create_cog_reader(infile) as cog:
+        with pytest.raises(TileNotFoundError):
+            tile = await cog.read(bounds=bounds, shape=(256,256))
+
+    monkeypatch.setattr(config, "BOUNDLESS_READ", True)
+    async with create_cog_reader(infile) as cog:
+        await cog.read(bounds=bounds, shape=(256,256))
+
+
+@pytest.mark.asyncio
+async def test_boundless_read_fill_value(create_cog_reader, monkeypatch):
+    infile = "http://async-cog-reader-test-data.s3.amazonaws.com/webp_web_optimized_cog.tif"
+    tile = mercantile.Tile(x=701, y=1634, z=12)
+    bounds = mercantile.xy_bounds(tile)
+
+
+    async with create_cog_reader(infile) as cog:
+        # Count number of pixels with a value of 1
+        tile = await cog.read(bounds=bounds, shape=(256,256))
+        counts = dict(zip(*np.unique(tile, return_counts=True)))
+        assert counts[1] == 5
+
+        # Set fill value of 1
+        monkeypatch.setattr(config, "BOUNDLESS_READ_FILL_VALUE", 1)
+
+        # Count number of pixels with a value of 1
+        tile = await cog.read(bounds=bounds, shape=(256,256))
+        counts = dict(zip(*np.unique(tile, return_counts=True)))
+        assert counts[1] == 179629
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "infile", TEST_DATA
+)
+async def test_read_not_in_bounds(create_cog_reader, infile):
+    tile = mercantile.Tile(x=0,y=0,z=25)
+    bounds = mercantile.xy_bounds(tile)
+
+    async with create_cog_reader(infile) as cog:
+        if cog.epsg != 3857:
+            bounds = transform_bounds(
+                "EPSG:3857",
+                f"EPSG:{cog.epsg}",
+                *bounds
+            )
+        with pytest.raises(TileNotFoundError):
+            await cog.read(bounds=bounds, shape=(256,256))
 
 
 @pytest.mark.asyncio
