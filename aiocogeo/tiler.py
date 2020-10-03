31c7d812ad924402ca9643d6c73af261e9c791ea
Jeff Albrecht <geospatialjeff@gmail.com>
2020-10-02 20:02:10 -0500

    working

--- aiocogeo/tiler.py
+++ aiocogeo/tiler.py
@@ -3,10 +3,12 @@ import asyncio
 from dataclasses import dataclass
 import math
 from typing import Any, Awaitable, Callable, Coroutine, Dict, List, Union, Optional, Sequence, Tuple, Type
+import warnings
 
 import numpy as np
 from PIL import Image
 
+from .constants import ColorInterp
 from .cog import COGReader
 
 
@@ -31,11 +33,14 @@ except ImportError:
 
 @dataclass
 class COGInfo:
-    min_zoom: int
-    max_zoom: int
-    bounds: List[float]
+    bounds: Tuple[float, float, float, float]
+    center: Tuple[float, float, int]
+    minzoom: int
+    maxzoom: int
     dtype: str
-    color_interp: str
+    colorinterp: List[str]
+    nodata_type: str
+    colormap: Optional[Dict[int, Sequence[int]]] = None
 
 
 @dataclass
@@ -85,18 +90,95 @@ class COGTiler(AsyncBaseReader):
         )
         return arr.astype(self.profile["dtype"])
 
+    async def info(self) -> COGInfo:
+        # TODO: Make sure this is in EPSG:3857 before getting resolution
+        mercator_resolution = max(
+            self.profile["transform"][0], abs(self.profile["transform"][4])
+        )
+        max_zoom = zoom_for_pixelsize(mercator_resolution)
+        min_zoom = zoom_for_pixelsize(
+            mercator_resolution
+            * max(self.profile["width"], self.profile["height"])
+            / 256
+        )
+
+        if self.cog.has_alpha:
+            nodata_type = "Alpha"
+        elif self.cog.is_masked:
+            nodata_type = "Mask"
+        elif self.cog.nodata is not None:
+            nodata_type = "Nodata"
+        else:
+            nodata_type = "None"
+
+        # TODO: Figure out where scale, offset, band_metadata, and band_descriptions come from
+        return COGInfo(
+            bounds=self.bounds,
+            center=self.center,
+            minzoom=min_zoom,
+            maxzoom=max_zoom,
+            dtype=self.profile["dtype"],
+            colorinterp=[color.name for color in self.cog.color_interp],
+            nodata_type=nodata_type,
+            colormap=self.cog.colormap
+        )
+
+    async def stats(
+        self,
+        pmin: float = 2.0,
+        pmax: float = 98.0,
+        hist_options: Optional[Dict] = None,
+        indexes: Optional[Union[Sequence[int], int]] = None,
+        width: Optional[int] = None,
+        height: Optional[int] = None,
+        max_size: int = 1024,
+        bounds: Optional[Tuple[float, float, float, float]] = None,
+        bounds_crs: CRS = CRS.from_epsg(4326),
+        resample_method: int = Image.NEAREST
+    ) -> Dict:
+
+        hist_options = hist_options or {}
+
+        if self.cog.colormap and not "bins" not in hist_options:
+            hist_options["bins"] = [
+                k for k, v in self.cog.colormap.items() if v != (0, 0, 0, 255)
+            ]
+
+        if isinstance(indexes, int):
+            indexes = (indexes,)
+
+        if indexes is None:
+            indexes = [idx for idx, b in enumerate(self.cog.color_interp) if b != ColorInterp.alpha]
+            if len(indexes) != self.profile['count']:
+                warnings.warn(
+                    "Alpha band was removed from the output data array"
+                )
+            indexes = range(self.profile['count'])
+
+        if bounds:
+            data = await self.part(bounds, bounds_crs=bounds_crs, width=width, height=height, resample_method=resample_method)
+        else:
+            data = await self.preview(width=width, height=height, max_size=max_size, resample_method=resample_method)
+
+        data = np.ma.array(data)
+
+        return {
+            indexes[b]: raster_stats(data[b], percentiles=(pmin, pmax), **hist_options)
+            for b in range(data.shape[0])
+        }
+
     async def tile(
         self,
-        x: int,
-        y: int,
-        z: int,
-        tile_size: int = 256,
-        tms: TileMatrixSet = DEFAULT_TMS,
+        tile_x: int,
+        tile_y: int,
+        tile_z: int,
+        tilesize: int = 256,
         resample_method: int = Image.NEAREST,
-    ) -> np.ndarray:
-        tile = morecantile.Tile(x=x, y=y, z=z)
+        tms: TileMatrixSet = DEFAULT_TMS,
+    ) -> Coroutine[Any, Any, Tuple[np.ndarray, np.ndarray]]:
+        tile = morecantile.Tile(x=tile_x, y=tile_y, z=tile_z)
         tile_bounds = tms.xy_bounds(tile)
-        width = height = tile_size
+        width = height = tilesize
         if self.cog.epsg != tms.crs:
             arr = await self._warped_read(
                 tile_bounds,
@@ -109,33 +191,18 @@ class COGTiler(AsyncBaseReader):
             arr = await self.cog.read(
                 tile_bounds, shape=(width, height), resample_method=resample_method
             )
-        return arr
-
-
-    async def point(
-        self,
-        coords: Tuple[float, float],
-        coords_crs: CRS = WGS84,
-        **kwargs: Any
-    ) -> np.ndarray:
-        if coords_crs != self.cog.epsg:
-            coords = [pt[0] for pt in transform_coords(
-                coords_crs, CRS.from_epsg(self.cog.epsg), [coords[0]], [coords[1]]
-            )]
-        arr = await self.cog.point(*coords)
-        return arr
-
+        return arr.data, arr.mask
 
     async def part(
         self,
-        bounds: Tuple[float, float, float, float],
-        bounds_crs: CRS = WGS84,
+        bbox: Tuple[float, float, float, float],
+        bbox_crs: CRS = WGS84,
         width: int = None,
         height: int = None,
         resample_method: int = Image.NEAREST
     ) -> np.ndarray:
-        if bounds_crs != self.cog.epsg:
-            bounds = transform_bounds(bounds_crs, CRS.from_epsg(self.cog.epsg), *bounds)
+        if bbox_crs != self.cog.epsg:
+            bounds = transform_bounds(bbox_crs, CRS.from_epsg(self.cog.epsg), *bbox)
 
         if not height or not width:
             width = math.ceil((bounds[2] - bounds[0]) / self.profile['transform'].a)
@@ -169,66 +236,16 @@ class COGTiler(AsyncBaseReader):
             resample_method=resample_method,
         )
 
-    async def info(self) -> COGInfo:
-        wgs84_bounds = transform_bounds(
-            CRS.from_epsg(self.cog.epsg), CRS.from_epsg(4326), *self.cog.bounds
-        )
-        mercator_resolution = max(
-            self.profile["transform"][0], abs(self.profile["transform"][4])
-        )
-        max_zoom = zoom_for_pixelsize(mercator_resolution)
-        min_zoom = zoom_for_pixelsize(
-            mercator_resolution
-            * max(self.profile["width"], self.profile["height"])
-            / 256
-        )
-
-        return COGInfo(
-            min_zoom=min_zoom,
-            max_zoom=max_zoom,
-            bounds=list(wgs84_bounds),
-            dtype=self.profile["dtype"],
-            color_interp=self.profile["photometric"],
-        )
-
-    async def stats(
-        self,
-        pmin: float = 2.0,
-        pmax: float = 98.0,
-        indexes: Optional[Union[Sequence[int], int]] = None,
-        width: int = None,
-        height: int = None,
-        max_size: int = 1024,
-        bounds: Optional[Tuple[float, float, float, float]] = None,
-        bounds_crs: CRS = CRS.from_epsg(4326),
-        resample_method: int = Image.NEAREST
-    ) -> Dict:
-
-        if isinstance(indexes, int):
-            indexes = (indexes,)
-
-        if indexes is None:
-            # TODO: Remove alpha band
-            # indexes = non_alpha_indexes(src_dst)
-            # if indexes != src_dst.indexes:
-            #     warnings.warn(
-            #         "Alpha band was removed from the output data array", AlphaBandWarning
-            #     )
-            indexes = range(self.profile['count'])
-
-        if bounds:
-            data = await self.part(bounds, bounds_crs=bounds_crs, width=width, height=height, resample_method=resample_method)
-        else:
-            data = await self.preview(width=width, height=height, max_size=max_size, resample_method=resample_method)
-
-        data = np.ma.array(data)
-
-        # TODO: Support histogram options
-        hist_options = {}
-        return {
-            indexes[b]: raster_stats(data[b], percentiles=(pmin, pmax), **hist_options)
-            for b in range(data.shape[0])
-        }
+    async def point(
+        self, lon: float, lat: float, **kwargs: Any
+    ) -> Coroutine[Any, Any, List]:
+        coords = [lon, lat]
+        if self.cog.epsg != 4326:
+            coords = [pt[0] for pt in transform_coords(
+                CRS.from_epsg(4326), CRS.from_epsg(self.cog.epsg), [coords[0]], [coords[1]]
+            )]
+        arr = await self.cog.point(*coords)
+        return arr.tolist()
 
 
 
