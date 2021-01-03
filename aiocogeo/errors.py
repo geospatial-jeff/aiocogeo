06778601b17d50f1dfe3a15fdcaddb7cc32e1727
Jeff Albrecht <geospatialjeff@gmail.com>
2021-01-03 12:03:47 -0600

    fix errors

--- aiocogeo/errors.py
+++ aiocogeo/errors.py
@@ -2,28 +2,24 @@
 from dataclasses import dataclass
 
 
-@dataclass
 class CogReadError(Exception):
     """exception base class"""
 
     ...
 
 
-@dataclass
 class InvalidTiffError(CogReadError):
     """file is not a tiff"""
 
     ...
 
 
-@dataclass
 class TileNotFoundError(CogReadError):
     """tile not found"""
 
     ...
 
 
-@dataclass
 class MissingAssets(CogReadError):
     """asset not found (stac)"""
 
